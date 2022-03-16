e0397697f3ad56da5511be8f224dcb4218acfac1
lyleokoth <okothlyleochieng@gmail.com>
2022-03-16 21:43:42 +0300

    feat: connected the front-enfd and back-end.

--- api-server/api/routes.py
+++ api-server/api/routes.py
@@ -1,7 +1,14 @@
+from crypt import methods
 from api import app
+from datetime import datetime
 
 
 @app.route('/api')
 @app.route('/')
 def api_home():
     return 'Hello from flask react app!'
+
+
+@app.route('/api/time', methods=['GET'])
+def get_time():
+    return {'time': datetime.utcnow().strftime("%m/%d/%Y, %H:%M:%S")}
